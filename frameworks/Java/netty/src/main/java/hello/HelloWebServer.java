54a208ccf3c4bdb55a3eb2e0bed566713eb66c06
joeyleeeeeee97 <joeylee97@163.com>
2022-04-09 15:22:13 +0800

    virtual thread mode

--- frameworks/Java/netty/src/main/java/hello/HelloWebServer.java
+++ frameworks/Java/netty/src/main/java/hello/HelloWebServer.java
@@ -14,7 +14,9 @@ import io.netty.channel.epoll.EpollServerSocketChannel;
 import io.netty.channel.kqueue.KQueue;
 import io.netty.channel.kqueue.KQueueServerSocketChannel;
 import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.oio.OioEventLoopGroup;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.channel.socket.oio.OioServerSocketChannel;
 import io.netty.incubator.channel.uring.IOUring;
 import io.netty.incubator.channel.uring.IOUringChannelOption;
 import io.netty.incubator.channel.uring.IOUringEventLoopGroup;
@@ -26,9 +28,13 @@ public class HelloWebServer {
 
 	static {
 		ResourceLeakDetector.setLevel(Level.DISABLED);
+		virtualThread = Boolean.parseBoolean(System.getProperty("virtualThread", "false"));
+		oio = Boolean.parseBoolean(System.getProperty("preferoio", "false"));
 	}
 
 	private final int port;
+	private static final Boolean virtualThread;
+	private static final Boolean oio;
 
 	public HelloWebServer(int port) {
 		this.port = port;
@@ -36,7 +42,15 @@ public class HelloWebServer {
 
 	public void run() throws Exception {
 		// Configure the server.
-		if (IOUring.isAvailable()) {
+		if (virtualThread) {
+			if (oio) {
+				OioEventLoopGroup oioVirtualEventLoopGroup = new OioEventLoopGroup(0, Thread.ofVirtual().factory());
+				doRun(oioVirtualEventLoopGroup, OioServerSocketChannel.class, IoMultiplexer.JDK);
+			} else {
+				EventLoopGroup virtualThreadGroup = new NioEventLoopGroup(Thread.ofVirtual().factory());
+				doRun(virtualThreadGroup, NioServerSocketChannel.class, IoMultiplexer.JDK);
+			}
+		} else if (IOUring.isAvailable()) {
 			doRun(new IOUringEventLoopGroup(), IOUringServerSocketChannel.class, IoMultiplexer.IO_URING);
 		} else
 			if (Epoll.isAvailable()) {
